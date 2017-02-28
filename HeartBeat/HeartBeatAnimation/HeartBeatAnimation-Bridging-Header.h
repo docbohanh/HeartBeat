aee15a24101e8f99f915cd26f203b748ee8c2a23
La Huu Thanh <taffy_nikolas@yahoo.com>
2017-02-28 16:46:29 +0700

    1

---
+++ HeartBeat/HeartBeatAnimation/HeartBeatAnimation-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <MXParallaxHeader/MXParallaxHeader.h>
+#import <MXParallaxHeader/MXScrollView.h>
+#import <MXParallaxHeader/MXScrollViewController.h>
+
+
