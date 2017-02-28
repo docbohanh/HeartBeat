933e78265dba5d38b42f17eca6cd60d3271e4ed0
La Huu Thanh <taffy_nikolas@yahoo.com>
2017-02-28 16:48:16 +0700

    1

---
+++ HeartBeat/HeartBeatAnimation/Libraries/PHExtensions.framework/Headers/PHExtensions iOS.h
@@ -0,0 +1,19 @@
+//
+//  PHExtensions iOS.h
+//  PHExtensions iOS
+//
+//  Created by Hoan Pham on 8/2/16.
+//  Copyright © 2016 Hoan Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PHExtensions iOS.
+FOUNDATION_EXPORT double PHExtensions_iOSVersionNumber;
+
+//! Project version string for PHExtensions iOS.
+FOUNDATION_EXPORT const unsigned char PHExtensions_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PHExtensions_iOS/PublicHeader.h>
+
+
